1eb3f53d4541cf4043880a2ed35e6fd3755ce5c0
makinivonic <makinivonic@gmail.com>
2025-01-24 03:59:47 +0300

    modified quiz.js

--- quiz.js
+++ quiz.js
@@ -1,42 +1,34 @@
-var choice1 = document.querySelector("#choice1");
-var choice2 = document.querySelector("#choice2");
-var choice3 = document.querySelector("#choice3");
 var feedback = document.querySelector("#feedback");
-
 var buttonSubmit = document.querySelector("#submit-answer");
 
-function checkAnswer(){
+function checkAnswer() {
+    // Retrieve the selected answer
+    const selectedAnswer = document.querySelector('input[name="quiz"]:checked'); // Select the checked radio button
 
-    if (choice1.checked){
-        var userAnswer = choice1.value;
-        return userAnswer;
+    if (selectedAnswer) {
+        return selectedAnswer.value; // Return the selected value
+    } else {
+        return null; // No option selected
     }
-    else if(choice2.checked){
-        var userAnswer = choice2.value;
-        return userAnswer;
-    }
-    else if (choice3.checked){
-        var userAnswer = choice3.value;
-        return userAnswer;
-    }
-
-    ;
-
 }
-function CorrectAnswer(answer){
-    correctAnswer = "4";
 
-    if (correctAnswer == answer){
+function CorrectAnswer(answer) {
+    const correctAnswer = "4"; // Define the correct answer
+
+    // Compare user answer with the correct answer
+    if (answer === correctAnswer) {
         feedback.innerHTML = "Correct! Well done.";
-    }
-    else{
+    } else {
         feedback.innerHTML = "That's incorrect. Try again!";
     }
 }
 
-buttonSubmit.addEventListener("click",function(){
-    var theAnswer = checkAnswer();
+buttonSubmit.addEventListener("click", function () {
+    const userAnswer = checkAnswer();
 
-    CorrectAnswer(theAnswer);
-
-})
+    if (userAnswer) {
+        CorrectAnswer(userAnswer); // Pass the user's answer to the function
+    } else {
+        feedback.innerHTML = "Please select an answer before submitting."; // Prompt to select an option
+    }
+});
